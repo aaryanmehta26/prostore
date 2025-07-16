aaeca84bb76bafcfdcd2aa78383d5f2767e8aee0
Aaryan Mehta <aaryanmehta2699@gmail.com>
2025-07-16 23:01:26 +0930

    added server action

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import sampleData from "@/db/sample-data";
 import ProductList from "@/components/shared/product/product-list";
+import { getLatestProducts } from "@/lib/actions/product.actions";
 
-const HomePage = () => {
-  console.log(sampleData);
+const HomePage = async () => {
+  const latestProducts = await getLatestProducts();
+  console.log("@@@@", latestProducts);
   return (
     <>
-      <ProductList data={sampleData.products} title="New Arrivals" limit={4} />
+      <ProductList data={latestProducts} title="New Arrivals" limit={4} />
     </>
   );
 };
