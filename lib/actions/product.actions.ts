c8b3cab384738cb8f2a085c15e266c728ee845a4
Aaryan Mehta <aaryanmehta2699@gmail.com>
2025-07-25 12:04:41 +0930

    completed product detail page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -7,11 +7,17 @@ import { convertToPlainObject } from "../utils";
 // Get latest products
 export async function getLatestProducts() {
   //   const prisma = new PrismaClient();
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: "desc" },
   });
 
   return convertToPlainObject(data);
+}
+
+// Get product by slug
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
 }
